eb46e93d5e60e4c315459ec43e2e9b364091bcdf
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2020-08-09 19:29:24 +0200

    Add german Localisation

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -4,30 +4,32 @@ import backend.data.DataHandler;
 import backend.dataclasses.ShoppingList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 
 public class Main extends Application {
 
     private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException{
-        scene = new Scene(loadFXML("Window"));
-        stage.setTitle("RecipeManager");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-    }
-
+    /**
+     * Loads fxml from path with german Localisation
+     *
+     * @param fxml name of fxml resource
+     * @return loaded object hierarchy from fxml document.
+     * @throws IOException If resource not found
+     */
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        Locale locale = new Locale("de", "DE");
+        String resourcePath = "RecipeManager";
+        ResourceBundle bundle = ResourceBundle.getBundle(resourcePath, locale);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"), bundle);
         return fxmlLoader.load();
     }
 
@@ -44,6 +46,14 @@ public class Main extends Application {
         launch();
 
 
+    }
 
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("Window"));
+        stage.setTitle("RecipeManager");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
