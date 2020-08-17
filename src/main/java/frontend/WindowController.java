9c90c9fa02609db73498827900cc3d047ad4f831
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2020-08-17 19:38:02 +0200

    Implement new recipe tab
    Add recipe tab to main Window.fxml
    If recipe is selected --> data is loaded into the recipe tab
    
    + new translations

--- src/main/java/frontend/WindowController.java
+++ src/main/java/frontend/WindowController.java
@@ -2,13 +2,13 @@ package frontend;
 
 import backend.dataclasses.recipe.Recipe;
 import backend.dataclasses.recipe.Recipes;
+import backend.dataclasses.recipecategories.RecipeCategory;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
@@ -34,6 +34,55 @@ public class WindowController {
     private TableColumn<Recipe, Integer> timeColumn; //time column " "
     @FXML
     private TableColumn<Recipe, URL> sourceColumn; //source column " "
+    @FXML
+    private Label recipeIDLabel; //ID label for shown recipe
+    @FXML
+    private TextField recipeNameTextField; //name textfield for shown recipe
+    @FXML
+    private ComboBox<RecipeCategory> recipeCategoryComboBox;
+    @FXML
+    private Button recipeSaveButton; //Button for saving changes/new recipe
+    @FXML
+    private Button recipeChangeButton; //Button for changing a recipe
+    @FXML
+    private Button recipeDeleteButton; //Button for deleting a recipe
+    @FXML
+    private TextField recipeTimeTextField; //Time textfield for shown recipe
+    @FXML
+    private CheckBox recipeVegetarianCheckbox; //Checkbox if recipe is vegetarian
+    @FXML
+    private TextField recipeSourceTextField; //textfield for shown recipe source
+
+    /**
+     * Initialization of window. Loads necessary data into the fxml components
+     * of the view.
+     */
+    @FXML
+    protected void initialize() {
+        loadRecipesIntoTable();
+    }
+
+    /**
+     * Loads the recipes into the table view.
+     */
+    private void loadRecipesIntoTable() {
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
+        sourceColumn.setCellValueFactory(new PropertyValueFactory<>("recipeLink"));
+
+        recipeTable.setItems(getRecipes());
+    }
+
+    /**
+     * Gets all saved recipes that are in the Singleton class of Recipes.
+     *
+     * @return list of all recipes
+     */
+    private ObservableList<Recipe> getRecipes() {
+        return Recipes.getInstance().getSavedRecipes();
+    }
 
     /**
      * Loads fxml from path with german Localisation.
@@ -48,28 +97,6 @@ public class WindowController {
         ResourceBundle bundle = ResourceBundle.getBundle(resourcePath, locale);
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"), bundle);
         return fxmlLoader.load();
-    }
-
-    /**
-     * Gets all saved recipes that are in the Singleton class of Recipes.
-     *
-     * @return list of all recipes
-     */
-    private ObservableList<Recipe> getRecipes() {
-        return Recipes.getInstance().getSavedRecipes();
-    }
-
-    /**
-     * Loads the recipes into the table view.
-     */
-    private void loadRecipesIntoTable() {
-        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
-        categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
-        timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
-        sourceColumn.setCellValueFactory(new PropertyValueFactory<>("recipeLink"));
-
-        recipeTable.setItems(getRecipes());
     }
 
     /**
@@ -108,11 +135,39 @@ public class WindowController {
     }
 
     /**
-     * Initialization of window. Loads necessary data into the fxml components
-     * of the view.
+     * When a recipe on the table is clicked then the data of the recipe is
+     * shown in the tab.
      */
-    @FXML
-    protected void initialize() {
-        loadRecipesIntoTable();
+    public void handleTableViewClickOnRecipe() {
+        activateRecipeButtons();
+        loadRecipeDataIntoTab();
     }
+
+    /**
+     * Activates the three buttons for a recipe: save, change, new.
+     */
+    private void activateRecipeButtons() {
+        recipeSaveButton.setDisable(false);
+        recipeChangeButton.setDisable(false);
+        recipeDeleteButton.setDisable(false);
+    }
+
+    /**
+     * Loads the data of the recipe into the tab.
+     */
+    private void loadRecipeDataIntoTab() {
+        Recipe selectedRecipe = recipeTable.getSelectionModel().getSelectedItem();
+
+        recipeIDLabel.setText(selectedRecipe.getId());
+        recipeNameTextField.setText(selectedRecipe.getTitle());
+
+        recipeTimeTextField.setText(Integer.toString(selectedRecipe.getTime()));
+        if (selectedRecipe.isVegetarian()) {
+            recipeVegetarianCheckbox.isSelected();
+        }
+        recipeSourceTextField.setText(selectedRecipe.getRecipeLink().toString());
+
+    }
+
+
 }
