fcaaa3a8f2d67fbb523309cbd1027ef0599e3d2d
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2020-10-05 18:14:46 +0200

    Refactor tooltip to its own class
    Remove ErrorTooltip from GroceryItemsWindowController and RecipeCategoriesWindowController

--- src/main/java/frontend/GroceryItemsWindowController.java
+++ src/main/java/frontend/GroceryItemsWindowController.java
@@ -309,30 +309,4 @@ public class GroceryItemsWindowController {
         groceryItemTextField.requestFocus();
     }
 
-    /**
-     * Represents extended Tooltip class for Error label when user makes faulty
-     * inputs.
-     */
-    private class ErrorTooltip extends Tooltip {
-
-        ErrorTooltip() {
-            this.setShowDelay(Duration.millis(300));
-        }
-
-        public void clearText() {
-            this.setText("");
-        }
-
-        public void addErrorMessage(String newError) {
-            String existingErrorMessage = this.getText();
-            String newErrorMessage;
-            if (existingErrorMessage.length() > 0) {
-                newErrorMessage = existingErrorMessage + "\n" + newError;
-            } else {
-                newErrorMessage = newError;
-            }
-            this.setText(newErrorMessage);
-        }
-
-    }
 }
